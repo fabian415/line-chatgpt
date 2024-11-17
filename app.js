16786a16c84de2b80b50b3315847fd8444473967
fabian.chung <fabian.chung@advantech.com.tw>
2024-11-17 18:00:15 +0800

    update

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ require("dotenv").config();
 
 const line = require("@line/bot-sdk");
 const express = require("express");
-const { Configuration, OpenAIApi } = require("openai");
+// const { Configuration, OpenAIApi } = require("openai");
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+const OpenAI = require("openai");
+
+const openAIclient = new OpenAI({
+  apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
 });
-const openai = new OpenAIApi(configuration);
 
 // create LINE SDK config from env variables
 const config = {
@@ -33,6 +38,16 @@ app.post("/callback", line.middleware(config), (req, res) => {
     });
 });
 
+// Test
+app.get("/hi", async (req, res) => {
+  const completion = await openAIclient.chat.completions.create({
+    messages: [{ role: "user", content: "你好嗎？" }],
+    model: "gpt-3.5-turbo",
+  });
+  const answer = completion.choices[0].message.content;
+  res.send(answer);
+});
+
 // event handler
 async function handleEvent(event) {
   if (event.type !== "message" || event.message.type !== "text") {
@@ -46,13 +61,19 @@ async function handleEvent(event) {
   // // use reply API
   // return client.replyMessage(event.replyToken, echo);
 
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: event.message.text,
+  // const completion = await openai.createCompletion({
+  //   model: "text-davinci-003",
+  //   prompt: event.message.text,
+  // });
+
+  const completion = await openAIclient.chat.completions.create({
+    messages: [{ role: "user", content: event.message.text }],
+    model: "gpt-3.5-turbo",
   });
 
+  const text = completion.choices[0].message.content;
   // create a echoing text message
-  const echo = { type: "text", text: completion.data.choices[0].text };
+  const echo = { type: "text", text: text };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
